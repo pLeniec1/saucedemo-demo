a34a031f07075d0a5b3d6e10ea73d423c421257b
Paweł Leniec <54355369+pLeniec1@users.noreply.github.com>
2024-10-29 22:22:49 +0100

    Update login.spec.ts

--- tests/specs/login.spec.ts
+++ tests/specs/login.spec.ts
@@ -13,38 +13,4 @@ test('Sign in with correct email', async ({ page, loginPage }) => {
     await expect(page.locator('[data-test="title"]')).toBeVisible()
 });
 
-test.skip('Sign in with incorrect username', async ({ page, loginPage }) => {
-    // expect error
-});
 
-test.skip('Sign in with incorrect password', async ({ page, loginPage }) => {
-    // expect error
-});
-
-test.skip('Sign in with empty password', async ({ page, loginPage }) => {
-    // expect error
-});
-
-test.skip('Sign in with empty username', async ({ page, loginPage }) => {
-    // expect error
-});
-
-test.skip('Sign in with empty username and password', async ({ page, loginPage }) => {
-    // expect error
-});
-
-test.skip('SQL injection', async ({ page, loginPage }) => {
-    // expect error
-});
-
-test.skip('different user/s', async ({ page, loginPage }) => {
-    // expect user with different role 
-});
-
-test.skip('username character limit (if any)', async ({ page, loginPage }) => {
-    // expect error
-});
-
-test.skip('password character limit (if any)', async ({ page, loginPage }) => {
-    // expect error
-});
